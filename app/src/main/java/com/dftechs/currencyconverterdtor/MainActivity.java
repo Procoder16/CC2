52e8161a55ebf7d868f3be3fa5989490d5edd552
Soumik Mukherjee <smsoumik1608@gmail.com>
2020-09-20 14:23:38 +0530

    New Release and Bug Fixes

--- app/src/main/java/com/dftechs/currencyconverterdtor/MainActivity.java
+++ app/src/main/java/com/dftechs/currencyconverterdtor/MainActivity.java
@@ -3,8 +3,30 @@ package com.dftechs.currencyconverterdtor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view){
+
+        Log.i("Info","Button Pressed");
+
+        EditText input= (EditText) findViewById(R.id.amount);
+
+        String amountInDollars=input.getText().toString();
+
+        double amountInDollarsDouble= Double.parseDouble(amountInDollars);
+
+        double amountInRupeesDouble= amountInDollarsDouble*73.61;
+
+        String amountInRupeesString = String.format("%.2f",amountInRupeesDouble);
+
+        Toast.makeText(this, "$ " + amountInDollars + " is " + amountInRupeesString + " rupees", Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
